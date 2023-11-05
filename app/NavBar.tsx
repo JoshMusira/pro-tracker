f60e5de45e4db30687f674ff9052481f5534285b
JoshMusira <joshuamusira01@gmail.com>
2023-11-05 10:36:35 +0300

    feat:fix bug

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link"
+import Image from 'next/image';
+import logo from '../public/assets/fixes.png'
 
 const NavBar = () => {
     const links = [
@@ -7,7 +9,14 @@ const NavBar = () => {
     ]
     return (
         <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
-            <Link href={'/'}>log</Link>
+            <Link href={'/'}>
+                <Image
+                    src={logo}
+                    alt="Logo"
+                    width={35}
+                    height={35}
+                />
+            </Link>
             <ul className="flex space-x-6">
                 {
                     links.map((link) => <Link key={link.href} className="text-zinc-500 hover:text-zinc-800 transition-colors" href={link.href}>{link.label}</Link>)
