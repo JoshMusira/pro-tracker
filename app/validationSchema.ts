8f44f8b13d9a103485c0a347a124d5cff15d620e
JoshMusira <joshuamusira01@gmail.com>
2023-11-12 15:22:13 +0300

    feat:create user schema

--- app/validationSchema.ts
+++ app/validationSchema.ts
@@ -4,3 +4,10 @@ export const createIssueSchema = z.object({
     title: z.string().min(1, 'Tittle is required').max(255),
     description: z.string().min(1, 'Description is required').max(65535),
 });
+
+
+export const createUserSchema = z.object({
+    name: z.string().min(1, 'Name is required'),
+    username: z.string().min(1, 'Username is required'),
+    email: z.string().email(),
+});
